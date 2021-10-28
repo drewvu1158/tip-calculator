e88e9c384b0c1b17cc044df7b0e6e472d55f5197
drewvu1158 <88225405+drewvu1158@users.noreply.github.com>
2021-10-27 21:29:49 -0700

    init commit

---
+++ resturant-tip.py
@@ -0,0 +1,11 @@
+# Restaurant Tip Calculator 
+# Create a program that allows the user to enter the price of a meal at a restaurant. 
+# The program calculates the amount of the tip to be paid at 20%. 
+# The tip and total price are then displayed separately.
+
+# EXTRA CREDIT
+# Ask the user how much percentage they would like to tip?
+
+# DOUBLE EXTRA CREDIT
+# Put your script in a Github repository, call it 'Tip Calculator'
+
